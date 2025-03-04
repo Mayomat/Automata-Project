f12bbf8ae37218483c3c9f0ba665864df1ce3adb
Paul b <paul.balaine@efrei.net>
2025-03-04 11:44:25 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# go through each transition and look if there is a transition
+# that have no more than n transition with a different language or if there is 1 entry
+def is_deterministic(fa):
+    pass
+
+
+# check if there is the same number of transitions that element
+# of the language per states with different element language
+def is_complete(fa):
+    pass
+
+
+# check that there is no transition going to the entry (only 1 entry)
+def is_standard(fa):
+    pass
+
+
+def determinization(fa):
+    pass
+
+
+def completing(fa):
+    pass
+
+
+def standardization(fa):
+    if not is_standard(fa):
+        pass
+    return
+
+
