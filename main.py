ff67f2bbfc350e67129a16f62e03d84bbaa004e3
remip <remi.pyt@gmail.com>
2025-03-17 16:32:13 +0100

    We can now choose the automata

--- main.py
+++ main.py
@@ -13,20 +13,6 @@ print(eg.is_standardized())
 print(eg.find_non_accessible_states())
 """
 
-
-ex = Automata()
-ex.create_automaton_from_file('eg.txt')
-ex.create_table()
-ex.display_transition_table()
-ex.fill_transition_table()
-ex.display_transition_table()
-ex.standardize()
-ex.create_table()
-ex.fill_transition_table()
-ex.display_transition_table()
-print(ex.max_transitions(1))
-ex.display_table()
-
 def print_menu():
     print("\n")
     print("╔════════════════════════════════════════════════════╗")
@@ -39,39 +25,53 @@ def print_menu():
     print("║ 4: Standardize Automaton                           ║")
     print("║ 5: Max Transitions from State 1                    ║")
     print("║ 6: Display Full Table                              ║")
-    print("║ 7: Exit                                            ║")
+    print("║ 7: Return to the automaton selection               ║")
     print("╚════════════════════════════════════════════════════╝")
 
 def main():
     ex = Automata()
 
     while True:
-        print_menu()
-        choice = int(input(">> Enter your choice: "))
-
-        if choice == 0:
-            ex.create_automaton_from_file('eg.txt')
-        if choice == 0:
-            ex.create_automaton_from_file('eg.txt')
-        elif choice == 1:
-            ex.create_table()
-        elif choice == 2:
-            ex.display_transition_table()
-        elif choice == 3:
-            ex.fill_transition_table()
-        elif choice == 4:
-            ex.standardize()
-        elif choice == 5:
-            print(f">> Result: {ex.max_transitions(1)}")
-        elif choice == 6:
-            ex.display_table()
-        elif choice == 7:
+        print("╔════════════════════════════════════════════════════╗")
+        print("║                AUTOMATON CONSOLE MENU              ║")
+        print("╠════════════════════════════════════════════════════╣")
+        print("║ Enter the number of the automaton that you want,   ║")
+        print("║ From 1 to 44 and O to exit                         ║")
+        print("╚════════════════════════════════════════════════════╝")
+        choice = int(input("Enter your choice: "))
+        if choice > 0 and choice < 45:
+            path = "Automata/"+str(choice)+".txt"
+        elif choice == 0:
             print("╔════════════════════════════════════════════════════╗")
             print("║                     ByeBye                         ║")
             print("╚════════════════════════════════════════════════════╝")
             break
         else:
             print("Invalid input. Please enter a number.")
+        while True:
+            print_menu()
+            choice = int(input("Enter your choice: "))
+
+            if choice == 0:
+                ex.create_automaton_from_file(path)
+            if choice == 0:
+                ex.create_automaton_from_file(path)
+            elif choice == 1:
+                ex.create_table()
+            elif choice == 2:
+                ex.display_transition_table()
+            elif choice == 3:
+                ex.fill_transition_table()
+            elif choice == 4:
+                ex.standardize()
+            elif choice == 5:
+                print(f"Result: {ex.max_transitions(1)}")
+            elif choice == 6:
+                ex.display_table()
+            elif choice == 7:
+                break
+            else:
+                print("Invalid input. Please enter a number.")
 
 
 if __name__ == "__main__":
