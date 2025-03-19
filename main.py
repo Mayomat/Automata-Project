c657faf73c3025764f134ddbd84c1a2518c7ac9e
Jeremy <jeremy.sagnard@gmail.com>
2025-03-19 16:20:59 +0100

    Word recognition from Paul

--- main.py
+++ main.py
@@ -2,37 +2,8 @@ from Automata import *
 
 # create and display the automata from "eg.txt"
 
-"""
-eg = Automata()
-eg.create_automaton_from_file('eg.txt')
-eg.create_table()
-eg.fill_transition_table()
-eg.display_transition_table()
-eg.is_deterministic()
-print(eg.is_standardized())
-print(eg.find_non_accessible_states())
-"""
-
 
 ex = Automata()
 ex.create_automaton_from_file('eg.txt')
-ex.create_table()
-ex.display_transition_table()
-ex.fill_transition_table()
-ex.display_transition_table()
-ex.standardize()
-ex.create_table()
-ex.fill_transition_table()
-ex.display_transition_table()
-print(ex.max_transitions(1))
 ex.display_table()
-
-
-"""
-print("[")
-for i in range(len(ex.initial[0].transitions)):
-    print("[", end="")
-    for j in range(len(ex.initial[0].transitions[i])):
-        print(eg.transition_table[i][j], end="")
-    print("]", end="")
-"""
+ex.word_recognition()
